1c6de109574e9b2e48955a1377651bf625ff264f
Rudranil Jadhav <45rudraniljadhav@gmail.com>
2026-03-06 01:10:03 +0530

    fix

--- agents/client.py
+++ agents/client.py
@@ -8,7 +8,8 @@ load_dotenv()
 credential = AzureCliCredential()
 
 client = AzureOpenAIChatClient(
-    azure_endpoint=os.environ["AZURE_AI_PROJECT_ENDPOINT"],
-    deployment_name=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
-    credential=credential,
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    deployment_name=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
+    api_version=os.environ["AZURE_OPENAI_API_VERSION"],
 )
